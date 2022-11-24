8d2a30ef8d51812e180aa34321afe3422cb9ae5d
irene <irene>
2022-11-24 16:11:24 +0000

    Saved recipe: DKU_CHURN.compute_data_out

--- recipes/compute_data_out.py
+++ recipes/compute_data_out.py
@@ -1,3 +1,9 @@
 import dataiku
 df = dataiku.Dataset("data").get_dataframe()
 print(df.isin(["Infinity", "-Infinity"]).sum())
+
+out_df = df.isin(["Infinity", "-Infinity"]).sum()
+
+# Write recipe outputs
+out = dataiku.Dataset("out")
+out.write_with_schema(out_df)
