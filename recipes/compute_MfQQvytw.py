adfd2dfd5e815c6c615f5e41737c57237c55734e
admin <admin>
2023-02-27 14:44:55 +0000

    Saved recipe: DKU_CHURN.compute_MfQQvytw

--- recipes/compute_MfQQvytw.py
+++ recipes/compute_MfQQvytw.py
@@ -4,11 +4,11 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-#data = dataiku.Dataset("data")
-#data_df = data.get_dataframe()
+data = dataiku.Dataset("data")
+data_df = data.get_dataframe()
 #
-#path = dataiku.Folder("s3_out").get_path()
-#full_path = path + '/models/top2vec_model_{market}'
+path = dataiku.Folder("s3_out").get_path()
+full_path = path + '/models/top2vec_model_{market}'
 
-#csv = data_df.to_csv(full_path)
+csv = data_df.to_csv(full_path)
 
