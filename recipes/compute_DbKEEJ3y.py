f433b310c2d8de9458e458f5aeda5a3feed4e518
irene <irene>
2023-02-27 14:48:19 +0000

    Saved recipe: DKU_CHURN.compute_DbKEEJ3y

---
+++ recipes/compute_DbKEEJ3y.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+data = dataiku.Dataset("data")
+data_df = data.get_dataframe()
+#
+path = dataiku.Folder("fs_out").get_path()
+full_path = path + 'top2vec_model.csv'
+
+csv = data_df.to_csv(full_path)
