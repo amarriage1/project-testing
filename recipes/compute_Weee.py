f0f88513f60ac6df1883489cc2b380d616abc6bc
irene <irene>
2023-02-28 15:03:47 +0000

    Saved recipe: DKU_CHURN.compute_Weee

--- recipes/compute_Weee.py
+++ recipes/compute_Weee.py
@@ -7,6 +7,8 @@ from top2vec import Top2Vec
 import warnings
 warnings.filterwarnings("ignore")
 
+model = Top2Vec(documents, embedding_model='distiluse-base-multilingual-cased')
+
 # Read recipe inputs
 unlabeled_customers = dataiku.Dataset("unlabeled_customers")
 unlabeled_customers_df = unlabeled_customers.get_dataframe()
@@ -23,9 +25,10 @@ data = dataiku.Dataset("unlabeled_customers")
 data_df = data.get_dataframe()
 #
 path = dataiku.Folder("s3_out").get_path()
-full_path = path + '/models/top2vec_model.csv'
+full_path = path + '/models/top2vec_model'
 
-csv = data_df.to_csv(full_path)
+csv = data_df.to_csv(full_path+'.csv')
+model.save(full_path)
 
 # Write recipe outputs
 Weee = dataiku.Dataset("Weee")
